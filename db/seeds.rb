ad24266d08d1acbc3343cd5c01c8eec1ba69046f
therunningagent <luistjr@gmail.com>
2021-01-25 13:59:29 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,5 @@
 User.create(username: "markymark", name: "mark")
 User.create(username: "louielou", name: "luis")
 
-Affirmation.create(affirmation: "lol")
-Affirmation.create(affirmation: "goodbye")
-
-Journal.create(user_id: 1, affirmation_id: 1, journal_entry: "journal entry 1", date: Time.now)
-Journal.create(user_id: 2, affirmation_id: 2, journal_entry: "journal entry 2", date: Time.now)
+Journal.create(user_id: 1, affirmation: "Affirmation 1", journal_entry: "journal entry 1", date: Time.now)
+Journal.create(user_id: 2, affirmation: "Affirmation 2", journal_entry: "journal entry 2", date: Time.now)
